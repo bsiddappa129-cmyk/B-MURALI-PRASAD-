dc0188a052839a30f7af49e4e3246ae6d089d1f6
bsiddappa129-cmyk <bsiddappa129@gmail.com>
2026-08-22 14:41:32 +0530

    Add files via upload

--- 9.py
+++ 9.py
@@ -1,131 +1,212 @@
-B MURALI PRASAD
-KUB25EEE610 
-21\08\2026
+# # # # NAME : B MURALI PRASAD
+# # # # USN : KUB25EEE610
+# # # 
+# # # DATE : 22/08/2026
 
+# # # nums = [3, 10, None, 15, 54, 75, 25, 23]
 
+# # # found = False
 
-'''numbers = [10, 3, 5, 6, 7, 8, 9, 24, 3, 5, 7, 89]
+# # # for num in nums:
+# # #     if num is not None and (num % 3 == 0 or num % 5 == 0 or num % 8 == 0):
+# # #         print(num)
+# # #         found = True
 
-even_numbers = []
-
-for num in numbers:
-    if num % 2 == 0:
-        even_numbers.append(num)
-
-print(even_numbers)'''
-
-'''text = "university"
-
-reversed_text = ""
-
-for char in text:
-    reversed_text = char + reversed_text
-
-print(reversed_text)'''
-
-'''numbers = [10, 3, 5, 6, 7, 8, 9, 24, 3, 5, 6, 7, 89]
-
-total = 0
-
-for num in numbers:
-    total += num
-
-average = total / len(numbers)
-
-print("Average:", average)'''
-
-'''numbers = [-1, 3, 34, -8, -9, 1]
-
-smallest = numbers[0]
-
-for num in numbers:
-    if num < smallest:
-        smallest = num
-
-print("Smallest number:", smallest)'''
-
-'''list1 = [1, 2, 3, 4]
-list2 = [3, 4, 5, 6]
-list3 = [4, 5, 6, 7]
-
-common = []
-
-for num in list1:
-    if num in list2 and num in list3:
-        common.append(num)
-
-print("Common elements:", common)'''
-
-'''numbers = [3, 10, 12, 54, 75, 89, 25, 23]
-
-for num in numbers:
-    if num % 3 != 0:
-        print(num)'''
-        
-'''numbers = [10, 3, 5, 6, 7, 8, 9, 24, 3, 5, 6, 7, 89]
-
-numbers.sort()
-
-print("Second smallest:", numbers[1])'''
-
-'''text = "university"
-
-count = 0
-
-for char in text:
-    count += 1
-
-print("Number of characters:", count)'''
-
-'''numbers = [-1, 3, 34, -8, -9, 1]
-
-numbers[0], numbers[-1] = numbers[-1], numbers[0]
-
-print(numbers)'''
-
-'''list1 = [1, 2, 3, 4]
-list2 = [3, 4, 5, 6]
-
-repeating = []
-
-for num in list1:
-    if num in list2:
-        repeating.append(num)
-
-print("Repeating values:", repeating)'''
-
-'''arr = [10, 3, 5, 6, 7, 8, 9, 24, 3, 6, 7, 89]
-
-smallest = min(arr)
-largest = max(arr)
-
-print("Smallest element:", smallest)
-print("Largest element:", largest)'''
-
-'''arr = [3, 10, 15, 54, 75, 89, 25, 23]
-
-for num in arr:
-    if num % 3 == 0 and num % 5 == 0:
-        print(num)'''
-
-'''arr = [-1, 3, 34, -8, -9, 1]
-
-arr[0], arr[2] = arr[2], arr[0]
-
-print(arr)'''
-
-'''list1 = [1, 2, 3, 4]
-list2 = [3, 4, 5, 6]
-
-result = list(set(list1) ^ set(list2))
-
-print(result)'''
-
-'''num = int(input("Enter a number: "))
-
-if num % 3 == 0:
-    print("Square:", num ** 2)
-else:
-    print("Number is not divisible by 3")'''
+# # # if not found:
+# # #     print("none")
     
+# # # nums = [10, 3, 5, 6, 7, 8, 9, 24, 3, 5, 6, 7, 89]
+
+# # # smallest = min(nums)
+# # # largest = max(nums)
+
+# # # print("Smallest:", smallest)
+# # # print("Largest:", largest)
+
+# # # nums = [-1, 3, 34, -8, -9, 1]
+
+# # # nums[0] = 100
+
+# # # print(nums)
+
+# # # list1 = [1, 2, 3, 4]
+# # # list2 = [3, 4, 5, 6]
+
+# # # avg = (sum(list1) + sum(list2)) / (len(list1) + len(list2))
+
+# # # print("Average:", avg)
+
+# # # num = int(input("Enter a number: "))
+
+# # # if num % 3 == 0:
+# # #     num = num + 5
+
+# # # print(num)
+
+# # # nums = [3, 10, 15, 54, 75, 25, 23]
+
+# # # for num in nums:
+# # #     if num % 3 == 0 and num % 5 != 0:
+# # #         print(num)
+
+# # # nums = [10, 3, 5, 6, 7, 8, 9, 24, 3, 5, 6, 7, 89]
+
+#  for num in nums:
+#     if num > 30:
+#          print(num)
+
+
+# nums = [-1, 3, 34, -8, -9, 1]
+
+# 1. Add an element
+# nums.append(10)
+# print("After append:", nums)
+
+# 2. Remove an element
+# nums.remove(34)
+# print("After remove:", nums)
+
+# 3. Insert an element
+# nums.insert(1, 20)
+# print("After insert:", nums)
+
+# 4. Find length
+# print("Length:", len(nums))
+
+# 5. Find maximum
+# print("Maximum:", max(nums))
+
+# 6. Find minimum
+# print("Minimum:", min(nums))
+
+#  7. Sort the list
+#  nums.sort()
+#  print("Sorted:", nums)
+
+#  8. Reverse the list
+#  nums.reverse()
+# print("Reversed:", nums)
+
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# avg = sum(nums) / len(nums)
+
+# print("Average:", avg)
+
+# num = 1578693
+# divisors = []
+
+# for i in range(1, 11):
+#     if num % i == 0:
+#         divisors.append(i)
+
+# print("Divisors:", divisors)
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+
+# if num1 % 5 == 0:
+#     num1 = num1 ** 2
+
+# if num2 % 5 == 0:
+#     num2 = num2 ** 2
+
+# print("First number:", num1)
+# print("Second number:", num2)
+
+# nums = [10, 3, 5, 6, 7, 8, 9, 24, 3, 5, 6, 7, 89]
+
+# prime = []
+# even = []
+# odd = []
+
+# for num in nums:
+#     # Even and odd
+#     if num % 2 == 0:
+#         even.append(num)
+#     else:
+#         odd.append(num)
+
+#     # Prime
+#     if num > 1:
+#         for i in range(2, num):
+#             if num % i == 0:
+#                 break
+#         else:
+#             prime.append(num)
+
+# print("Prime numbers:", prime)
+# print("Even numbers:", even)
+# print("Odd numbers:", odd)
+
+# nums = [-1, 3, 34, -8, -9, 1]
+
+# result = []
+
+# for num in nums:
+#     if num >= 0 and num % 3 != 0:
+#         result.append(num)
+
+# print(result)
+
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# total = sum(nums)
+# count = len(nums)
+# avg = total / count
+
+# print("Sum:", total)
+# print("Count:", count)
+# print("Average:", avg)
+
+# num = 1578693
+
+# for i in range(1, 11):
+#     if num % i == 0:
+#         num = num - 100
+#         print(i, "is divisible, result:", num)
+#     else:
+#         print(i, "is not divisible")
+
+
+# word = "university"
+
+# count = 0
+
+# for ch in word:
+#     if ch in "aeiou":
+#         count += 1
+
+# print("Vowel count:", count)
+
+# nums = [10, 3, 5, 6, 7, 8, 9, 24, 3, 5, 6, 7, 89]
+
+# # Print 89
+# print(89)
+
+# # Add 59 at 9th index
+# nums.insert(9, 59)
+
+# print(nums)
+
+# nums = [-1, 3, 34, -8, -9, 1]
+
+# for num in nums:
+#     print(num ** 2)
     
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+
+# result = num1 // num2
+
+# print("Floor division:", result)
+
+# nums = [10, 3, 5, 6, 7, 8, 9, 24, 3, 5, 6, 7, 89, 7, 8, 54, 621, 57, 24, 3, 5, 6, 4]
+
+# unique = []
+
+# for num in nums:
+#     if num not in unique:
+#         unique.append(num)
+
+# print("Unique values:", unique)  
